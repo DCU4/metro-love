c5930ee14dfa9f33b038de9efa03cd86afe18866
Dylan Connor <DylanJConnor4@gmail.com>
2025-10-16 20:41:23 -0400

    update front end url

--- src/components/MetroTimes.vue
+++ src/components/MetroTimes.vue
@@ -10,7 +10,7 @@ export default {
   },
   methods: {
     getMetroTimes(stationCode = 'F04') {
-      let url = 'https://metro-love.onrender.com/metro-timesmetro-times'
+      let url = 'https://metro-love.vercel.app/metro-times'
       if (location.hostname === 'localhost') {
         url = 'http://localhost:8081/metro-times'
       }
